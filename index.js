db4bfebe0fef0a30c98e2876c63db780203e5670
PS15A02\vantan <iroha_0416@yahoo.co.jp>
2017-05-26 19:17:56 +0900

    update

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var app = express();
 var port = 9999;
 
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json({extended: false}))
+
 app.use(express.static(__dirname + '/public'));
 
+var messages = [];
+
+app.post('/messages',function(req,res){
+	console.log(req.body.name);
+	console.log(req.body.message);
+	messages.push({name:req.body.name, message:req.body.message});
+	console.log(messages);
+	res.send('OK');
+});
+
+app.get('/messages',function(req,res){
+	res.send(messages);
+});
+
+app.listen(port, function(){
+	console.log('Server is listenig to 9999 part');
+});
+
+
+/*
 app.get('/scream',function(req,res){
 	res.send("あ^～");
 });
@@ -15,7 +39,4 @@ app.get('/now',function(req, res){
 app.get('/about',function(req,res){
 	res.send("What on earth are you talking about");
 });
-
-app.listen(port, function(){
-	console.log('Server is listenig to 9999 part');
-});
+*/
--- public/app.js
+++ public/app.js
@@ -1,4 +1,37 @@
-document.getElementById('startButton').addEventListener('click',function(){
+$('#sendmessage').click(sendMessage);
+
+function sendMessage(){
+	var name = $('#name').val();
+	var message = $('#message').val();
+	console.log(name +':'+ message);
+	if(!name || !message){
+		return;
+	}
+	$.ajax({
+		url: '/messages',
+		method: 'POST',
+		data: JSON.stringify({name: name, message: message}),
+		contentType: 'application/json',
+		success: function(response){
+			console.log(name + ':' + message);
+		}
+	});
+}
+
+setInterval(readMessage,1000);
+
+function readMessage(){
+	$.ajax({
+		url: '/messages',
+		method: 'GET',
+		success: function(response){
+			console.log(response);
+		}
+,	})
+}
+
+
+/*document.getElementById('startButton').addEventListener('click',function(){
 	$.ajax({
 		method: 'GET',
 		url: '/scream',
@@ -16,4 +49,4 @@ setInterval(function(){
 		$('#highlight').text(response);	
 		}
 	})
-},1);
+},1);*/
